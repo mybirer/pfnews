7172d3a97633f690974586ebc6490c9e13479a0e
MyBirer <mybirer@gmail.com>
2017-07-14 11:32:46 +0300

    little errors fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/pfnews/';
+$config['base_url'] = 'http://pfnews.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost:8080/pfnews/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ $config['uri_protocol']	= 'REQUEST_URI';
 |
 | https://codeigniter.com/user_guide/general/urls.html
 */
-$config['url_suffix'] = '.html';
+$config['url_suffix'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -324,7 +324,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'randomstring';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,14 +521,3 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
-
-/*
-  |--------------------------------------------------------------------------
-  | HMVC Modular
-  |--------------------------------------------------------------------------
-  |
-  | Modules location
- */
-$config['modules_locations'] = array(
-    APPPATH . 'modules/' => '../modules/',
-);
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -2,6 +2,7 @@
     class MY_Controller extends MX_Controller{
         function __construct(){
             parent::__construct();
+            $this->load->library('session');
         }
     }
 
